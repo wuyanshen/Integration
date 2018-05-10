32e17b49d6256be61fdd07a642bdea162d7c53c2
wuyanshen <763010522@qq.com>
2018-05-10 19:20:27 +0800

    thirty-ninth commit

--- elibus/message-service-bus/src/main/java/com/elisoft/servicebus/MessageServiceBus.java
+++ elibus/message-service-bus/src/main/java/com/elisoft/servicebus/MessageServiceBus.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.*;
 import org.springframework.integration.annotation.Gateway;
@@ -21,10 +22,11 @@ import org.springframework.integration.ws.WebServiceHeaders;
  * @author JiJie.LianG
  */
 @SpringBootApplication
+@EnableCaching
 @EnableAutoConfiguration //spring-boot 自动根据依赖配置
-@EnableIntegration  //启用 spring-integration
+//@EnableIntegration  //启用 spring-integration
 @IntegrationComponentScan //spring-integration DSL 特性需启动
-@ImportResource({"/META-INF/spring/integration/esb-conf-2.xml","/META-INF/spring/integration/ws-conf.xml","/META-INF/spring/integration/esb-conf.xml"}) //加载 spring-integration 业务集成配置
+@ImportResource({"classpath:/META-INF/spring/integration/esb-conf-2.xml","classpath:/META-INF/spring/integration/ws-conf.xml","classpath:/META-INF/spring/integration/esb-conf.xml"}) //加载 spring-integration 业务集成配置
 public class MessageServiceBus {
     public static void main(String[] args){
         SpringApplication.run(MessageServiceBus.class, args);
--- elibus/message-service-bus/src/main/java/com/elisoft/servicebus/modules/esb/web/EsbWebController.java
+++ elibus/message-service-bus/src/main/java/com/elisoft/servicebus/modules/esb/web/EsbWebController.java
@@ -3,13 +3,14 @@ package com.elisoft.servicebus.modules.esb.web;
 import com.elisoft.servicebus.core.EsbWsHeader;
 import com.elisoft.servicebus.core.gateway.EsbGateway;
 import com.elisoft.servicebus.core.gateway.EsbSyncGateway;
+import com.elisoft.servicebus.modules.esb.entity.WebService;
+import com.elisoft.servicebus.modules.esb.service.EsbWsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.integration.ws.WebServiceHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Component
 @RestController
@@ -34,6 +35,9 @@ public class EsbWebController {
 
     @Autowired
     private EsbSyncGateway syncGateway;
+
+    @Autowired
+    private EsbWsService esbWsService;
 
 //    @Autowired
 //    WsService wsService;
@@ -69,7 +73,32 @@ public class EsbWebController {
         headerMap.put(EsbWsHeader.SOAP_URL,soapUrl);
         headerMap.put("Content-Type", contentType);
         headerMap.put(WebServiceHeaders.SOAP_ACTION, soapAction);
+
+        WebService webService = new WebService();
+        webService.setId(UUID.randomUUID().toString());
+        webService.setSoapUrl(soapUrl);
+        webService.setSoapAction(soapAction);
+        webService.setReqInfo(reqXml);
+        webService.setCreateDate(new Date());
+        webService.setDelFlag("0");
+        esbWsService.saveWs(webService);
+
         Object wsResp = syncGateway.wsProcess(reqXml, headerMap);
+
+        webService.setRespInfo(wsResp.toString());
+        webService.setUpdateDate(new Date());
+        esbWsService.updateWs(webService);
+
+
+
         return wsResp;
+    }
+
+
+    @GetMapping("/all")
+    public Object findAll(){
+        //测试
+        List<WebService> list = esbWsService.findAll();
+       return list;
     }
 }
