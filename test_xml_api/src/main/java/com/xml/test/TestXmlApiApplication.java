9bf071aec73c0a5da631fb90c65b54ddd3330488
wuyanshen <763010522@qq.com>
2018-05-15 16:40:32 +0800

    fourtieth commit

--- test_xml_api/src/main/java/com/xml/test/TestXmlApiApplication.java
+++ test_xml_api/src/main/java/com/xml/test/TestXmlApiApplication.java
@@ -3,8 +3,10 @@ package com.xml.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.integration.config.EnableIntegration;
 
 @SpringBootApplication
+@EnableIntegration
 @ImportResource("classpath:integration.xml")
 public class TestXmlApiApplication {
 
--- test_xml_api/src/main/java/com/xml/test/controller/ReqController.java
+++ test_xml_api/src/main/java/com/xml/test/controller/ReqController.java
@@ -2,24 +2,33 @@ package com.xml.test.controller;
 
 import com.xml.test.api.HelloMsg;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.integration.ws.WebServiceHeaders;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.PollableChannel;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class ReqController {
 
-    @Autowired
+    /*@Autowired
     private MessageChannel requestChannel;
 
     @Autowired
-    private PollableChannel outputChannel;
+    private PollableChannel outputChannel;*/
 
-    @RequestMapping(path = "/welcome/{name}",method = RequestMethod.GET,produces = "application/json",consumes = "application/json")
+    @Autowired
+    @Qualifier(value = "wsinputChannel")
+    private MessageChannel wsinputChannel;
+
+  /*  @Autowired
+    @Qualifier(value = "output")
+    private PollableChannel output;*/
+
+    /*@RequestMapping(path = "/welcome/{name}",method = RequestMethod.GET,produces = "application/json",consumes = "application/json")
     public void sendMsg(@PathVariable String name){
         requestChannel.send(MessageBuilder.withPayload(name).build());
     }
@@ -28,5 +37,18 @@ public class ReqController {
     public void getMsg(){
         HelloMsg msg = (HelloMsg)outputChannel.receive(0).getPayload();
         System.out.println(msg.getMsg());
+    }*/
+
+    @RequestMapping(value = "/call2", method = RequestMethod.POST)
+    public String call2(@RequestHeader String soapAction, @RequestHeader String soapUrl, @RequestBody String reqXml) throws Exception {
+        MessageBuilder<String> stringMessageBuilder = MessageBuilder.withPayload(reqXml);
+        stringMessageBuilder.setHeader(WebServiceHeaders.SOAP_ACTION, soapAction);
+        stringMessageBuilder.setHeader("soapUrl", soapUrl);
+
+        wsinputChannel.send(stringMessageBuilder.build());
+       /* Message<?> receive = output.receive(180000);
+        String resXml = receive.getPayload().toString();
+        return resXml;*/
+       return "ok";
     }
 }
